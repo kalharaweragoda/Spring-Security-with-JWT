5046e188476bbde28068f8382e41fe4a14b955b3
kalharaweragoda <kalharaweragoda@gmail.com>
2026-03-25 23:10:03 +0530

    create AuthController file

--- src/main/java/edu/icet/controller/AuthController.java
+++ src/main/java/edu/icet/controller/AuthController.java
@@ -1,12 +1,59 @@
 package edu.icet.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import edu.icet.dto.JwtResponse;
+import edu.icet.dto.LoginRequest;
+import edu.icet.dto.SignupRequest;
+import edu.icet.entity.UserEntity;
+import edu.icet.util.JwtUtility;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 @CrossOrigin
 public class AuthController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtUtility jwtUtility;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtility.generateJwtToken((UserDetails) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new JwtResponse(jwt));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody SignupRequest signUpRequest) {
+        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return ResponseEntity.badRequest().body("Error: Username is already taken!");
+        }
+
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUsername(signUpRequest.getUsername());
+        userEntity.setPassword(passwordEncoder.encode(signUpRequest.getPassword()));
+        userEntity.setRole(signUpRequest.getRole());
+        userRepository.save(userEntity);
+
+        return ResponseEntity.ok("User registered successfully!");
+    }
 }
