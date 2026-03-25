0559f586573ca27edb2752392974044a1c4171cf
kalharaweragoda <kalharaweragoda@gmail.com>
2026-03-25 22:55:55 +0530

    create UserEntity add

---
+++ src/main/java/edu/icet/entity/UserEntity.java
@@ -0,0 +1,24 @@
+package edu.icet.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@ToString
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    private String role = "ROLE_USER";
+}
