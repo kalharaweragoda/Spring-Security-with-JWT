ebbe2346e3e6ec8bc42bef79dffbf7f1d4e60be5
kalharaweragoda <kalharaweragoda@gmail.com>
2026-03-25 23:04:54 +0530

    create JwtUtility file add

---
+++ src/main/java/edu/icet/util/JwtUtility.java
@@ -0,0 +1,43 @@
+package edu.icet.util;
+
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class JwtUtility {
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private long expirationMs;
+
+    public String generateJwtToken(UserDetails userDetails){
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis()+expirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public String getUsernameFromJwtToken(String token){
+        return Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJwt(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateJwtToken(String authToken){
+        try{
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJwt(authToken);
+            return true;
+        }catch (Exception e) {
+            log.error(e.getMessage());
+        }
+        return  false;
+    }
+}
